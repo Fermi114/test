3953dad38b14ad23d13a5703e04cdeb29f430acc
Fermi114 <119268398+Fermi114@users.noreply.github.com>
2022-12-01 18:55:43 +0300

    Update script.js

--- java/script.js
+++ java/script.js
@@ -1,6 +1,6 @@
-$(document).reay(function(){
+$(document).ready(function(){
     $('.header__burger').click(function(event){
         $('.header__burger, .header__menu').toggleClass('active');
         $('body').toggleClass('lock');
-    })
+    });
 });
